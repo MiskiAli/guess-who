fd53208e5db4ae0af1735b29417372b7361e0024
Miski Ali <miskiali222@gmail.com>
2022-06-20 16:54:00 -0500

    got all the pics to append on to the dom

--- client.js
+++ client.js
@@ -10,7 +10,8 @@ function appendDivs(){
     for (let i=0; i<people.length; i++){
     console.log(people[i]);
         // append pics to the dom
-        $('body').append('<img src="https://github.com/christopher-black.png?size=250" alt="Profile image of Chris"></img>')
+
+        $('body').append(`<img src="https://github.com/${people[i].githubUsername}.png?size=250" alt="Profile image of Chris"></img>`)
 
 }
 }
